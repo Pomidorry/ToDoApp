2ee5f133ce126f832407ab184133596e9e32d484
Pomidorry <106489913+Pomidorry@users.noreply.github.com>
2023-02-18 17:40:45 +0200

    changed to red

--- ToDoApp/ToDoApp/AddTask.xaml.cs
+++ ToDoApp/ToDoApp/AddTask.xaml.cs
@@ -43,7 +43,7 @@ namespace ToDoApp
             }
             else
             {
-                TaskBox.Background = Brushes.Magenta;
+                TaskBox.Background = Brushes.Red;
             }
         }
     }
